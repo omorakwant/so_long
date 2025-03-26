57d4b4ecbcb7a876e36944dad8c51a70e732d69b
omorakwant <125460156+omorakwant@users.noreply.github.com>
2025-03-26 23:21:15 +0000

    Update map_load.c

--- map_load.c
+++ map_load.c
@@ -12,119 +12,66 @@
 
 #include "so_long.h"
 
-void	load_player_image(t_data *data)
+int	rows_calc(char *map_file)
 {
-	data->player_up[0] = mlx_xpm_file_to_image(data->mlx,
-			"sprites/player_up.xpm", &data->width, &data->height);
-	data->player_up[1] = mlx_xpm_file_to_image(data->mlx,
-			"sprites/player_up_mv.xpm", &data->width, &data->height);
-	data->player_down[0] = mlx_xpm_file_to_image(data->mlx,
-			"sprites/player_down.xpm", &data->width, &data->height);
-	data->player_down[1] = mlx_xpm_file_to_image(data->mlx,
-			"sprites/player_down_mv.xpm", &data->width, &data->height);
-	data->player_left[0] = mlx_xpm_file_to_image(data->mlx,
-			"sprites/player_left.xpm", &data->width, &data->height);
-	data->player_left[1] = mlx_xpm_file_to_image(data->mlx,
-			"sprites/player_left_mv.xpm", &data->width, &data->height);
-	data->player_right[0] = mlx_xpm_file_to_image(data->mlx,
-			"sprites/player_right.xpm", &data->width, &data->height);
-	data->player_right[1] = mlx_xpm_file_to_image(data->mlx,
-			"sprites/player_right_mv.xpm", &data->width, &data->height);
-	if (!data->player_up[0] || !data->player_up[1] || !data->player_down[0]
-		|| !data->player_down[1] || !data->player_left[0]
-		|| !data->player_left[1] || !data->player_right[0]
-		|| !data->player_right[1])
+	int		i;
+	int		fd;
+	char	*line;
+
+	i = 0;
+	fd = open(map_file, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	line = get_next_line(fd);
+	while (line)
 	{
-		ft_putstr("Error loading player textures\n");
-		close_window(data);
+		i++;
+		free(line);
+		line = get_next_line(fd);
 	}
+	close(fd);
+	return (i);
 }
 
-void	load_enemy_image(t_data *data)
+char	**read_map_lines(int fd, t_data *data, int total_rows)
 {
-	data->enemy_img[0] = mlx_xpm_file_to_image(data->mlx, "sprites/enemy.xpm",
-			&data->width, &data->height);
-	data->enemy_img[1] = mlx_xpm_file_to_image(data->mlx,
-			"sprites/enemy_mv.xpm", &data->width, &data->height);
-	if (!data->enemy_img[0] || !data->enemy_img[1])
+	char	**map;
+	char	*line;
+	int		i;
+
+	map = malloc(sizeof(char *) * (total_rows + 1));
+	if (!map)
+		return (NULL);
+	i = 0;
+	line = get_next_line(fd);
+	if (!line)
 	{
-		ft_putstr("Error loading textures\n");
-		close_window(data);
+		free(map);
+		close(fd);
+		exit_error("Error\nMap is Empty");
 	}
+	data->columns = ft_strlen(line);
+	if (data->columns > 0 && line[data->columns - 1] == '\n')
+		data->columns--;
+	while (line)
+	{
+		map[i++] = line;
+		line = get_next_line(fd);
+	}
+	map[i] = NULL;
+	return (map);
 }
 
-void	load_image(t_data *data)
+char	**read_map(t_data *data)
 {
-	load_player_image(data);
-	load_enemy_image(data);
-	data->coin_img = mlx_xpm_file_to_image(data->mlx, "sprites/coin.xpm",
-			&data->width, &data->height);
-	data->wall_img = mlx_xpm_file_to_image(data->mlx, "sprites/wall.xpm",
-			&data->width, &data->height);
-	data->floor_img = mlx_xpm_file_to_image(data->mlx, "sprites/floor.xpm",
-			&data->width, &data->height);
-	data->exit_img = mlx_xpm_file_to_image(data->mlx, "sprites/exit.xpm",
-			&data->width, &data->height);
-	if (!data->wall_img || !data->coin_img || !data->exit_img
-		|| !data->floor_img)
-	{
-		ft_putstr("Error loading textures\n");
-		close_window(data);
-	}
-}
+	int		fd;
+	char	**map;
 
-void	render_image_norm(t_data *data, int y)
-{
-	int	x;
-
-	x = 0;
-	while (x < data->columns)
-	{
-		if (data->map[y][x] == '1')
-			mlx_put_image_to_window(data->mlx, data->window, data->wall_img, x
-				* 32, y * 32);
-		else if (data->map[y][x] == '0')
-			mlx_put_image_to_window(data->mlx, data->window, data->floor_img, x
-				* 32, y * 32);
-		else if (data->map[y][x] == 'C')
-			mlx_put_image_to_window(data->mlx, data->window, data->coin_img, x
-				* 32, y * 32);
-		else if (data->map[y][x] == 'E')
-			mlx_put_image_to_window(data->mlx, data->window, data->exit_img, x
-				* 32, y * 32);
-		else if (data->map[y][x] == 'X')
-			mlx_put_image_to_window(data->mlx, data->window,
-				data->enemy_img[data->enemy_frame], x * 32, y * 32);
-		mlx_put_image_to_window(data->mlx, data->window, data->floor_img, x
-			* 32, (data->rows) * 32);
-		x++;
-	}
-}
-
-void	render_image(t_data *data)
-{
-	int	y;
-
-	y = 0;
-	while (y < data->rows)
-	{
-		render_image_norm(data, y);
-		y++;
-	}
-	if (data->player_dir == 0)
-		mlx_put_image_to_window(data->mlx, data->window,
-			data->player_up[data->player_frame], data->player_y * 32,
-			data->player_x * 32);
-	else if (data->player_dir == 1)
-		mlx_put_image_to_window(data->mlx, data->window,
-			data->player_down[data->player_frame], data->player_y * 32,
-			data->player_x * 32);
-	else if (data->player_dir == 2)
-		mlx_put_image_to_window(data->mlx, data->window,
-			data->player_left[data->player_frame], data->player_y * 32,
-			data->player_x * 32);
-	else if (data->player_dir == 3)
-		mlx_put_image_to_window(data->mlx, data->window,
-			data->player_right[data->player_frame], data->player_y * 32,
-			data->player_x * 32);
+	fd = open(data->filename, O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	data->rows = rows_calc(data->filename);
+	map = read_map_lines(fd, data, data->rows);
+	close(fd);
+	return (map);
 }
